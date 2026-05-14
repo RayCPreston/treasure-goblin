a5ea83bfcca60a481554cbadb408f85bc6b5e074
Ray.Preston <rpreston@copaair.com>
2026-05-14 11:10:56 -0500

    Refine and enhancements for entity and world_grid

---
+++ project/autoloads/world_grid.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var _cells: Dictionary = {}
+var _tile_query: TileQuery = TileQuery.new()
+
+func initialize(layers: Array[TileMapLayer]) -> void:
+	_tile_query.initialize(layers)
+
+func register(entity: Entity, cell: Vector2i) -> void:
+	_cells[cell] = entity
+
+func unregister(cell: Vector2i) -> void:
+	_cells.erase(cell)
+
+func get_entity_at_cell(cell: Vector2i) -> Entity:
+	return _cells.get(cell, null)
+
+func is_occupied(cell: Vector2i) -> bool:
+	return _cells.has(cell)
+
+func is_cell_available(cell: Vector2i) -> bool:
+	return _tile_query.is_walkable(cell) and not is_occupied(cell)
+
+func move_entity(entity: Entity, from_cell: Vector2i, to_cell: Vector2i) -> void:
+	unregister(from_cell)
+	register(entity, to_cell)
+
+func swap_entities(entity_a: Entity, entity_b: Entity) -> void:
+	var cell_a := entity_a.cell
+	var cell_b := entity_b.cell
+	_cells[cell_a] = entity_b
+	_cells[cell_b] = entity_a
--- project/scripts/actors/entity.gd
+++ project/scripts/actors/entity.gd
@@ -14,19 +14,37 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	WorldGrid.unregister(cell)
 
-func interact(source: Entity) -> void:
+func interact(_source: Entity) -> void:
 	pass
 
 func take_turn() -> void:
 	pass
+
+func try_move_to(to_cell: Vector2i) -> void:
+	var occupant: Entity = WorldGrid.get_entity_at_cell(to_cell)
+	if occupant and occupant.is_passable:
+		swap_with(occupant)
+	elif occupant and occupant.is_interactable:
+		occupant.interact(self)
+	elif WorldGrid.is_cell_available(to_cell):
+		move_to(to_cell)
+	#TODO: Announce turn to TurnManager
 
 func move_to(to_cell: Vector2i) -> void:
 	WorldGrid.move_entity(self, cell, to_cell)
 	cell = to_cell
 	position = cell_to_world(cell)
 
-func world_to_cell(world_pos: Vector2) -> Vector2i:
-	return Vector2i(world_pos / Constants.Tile_Size)
+func swap_with(other: Entity) -> void:
+	var other_cell: Vector2i = other.cell
+	WorldGrid.swap_entities(self, other)
+	other.cell = cell
+	other.position = cell_to_world(cell)
+	cell = other_cell
+	position = cell_to_world(other_cell)
 
-func cell_to_world(cell: Vector2i) -> Vector2:
-	return Vector2(cell * Constants.TILE_SIZE)
+func world_to_cell(world_pos: Vector2) -> Vector2i:
+	return Vector2i(world_pos / Constants.TILE_SIZE)
+
+func cell_to_world(grid_pos: Vector2i) -> Vector2:
+	return Vector2(grid_pos * Constants.TILE_SIZE)
